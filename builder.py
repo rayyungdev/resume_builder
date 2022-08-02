ca419e4e54b30b2287a5a36c0990588592d0e6b0
Raymond <thec0okierebel12@gmail.com>
2022-08-02 00:07:24 -0400

    first commit

---
+++ builder.py
@@ -0,0 +1,117 @@
+from re import S
+import pandas as pd
+from templates import *
+
+month = {
+    1 : 'January', 2 : 'February', 3 : 'March', 4 : 'April', 5 : 'May', 6 : 'June', 7 : 'July', 8 : 'August', 9 : 'September', 10 : 'October', 11 : 'November',12 : 'December'    
+}
+
+class resume_builder:
+    def __init__(self, fjobs, fskills, flanguage, basic_info):
+        self.jobs = pd.read_csv(fjobs)
+        self.jobs['start'] = pd.to_datetime(self.jobs['start'])
+        self.jobs['end'] = pd.to_datetime(self.jobs['end'])
+        self.jobs = self.jobs.sort_values('start', ascending = False)
+        self.jobs = self.jobs
+        self.jobs.tags = self.jobs.tags.apply(lambda x: x.split(', '))
+        self.jobs.detail = self.jobs.detail.apply(lambda x: x.split(', '))
+        
+        self.skills = pd.read_csv(fskills)
+        # for col in self.skills.columns:
+        #     self.skills[col] = self.skills[col].apply(lambda x: str(x).split(', '))
+
+        self.language = pd.read_csv(flanguage)
+        for col in self.language.columns:
+            self.language[col] = self.language[col].apply(lambda x: str(x).split(', '))
+
+        self.basic = pd.read_csv(basic_info)
+        self.basic.edu_1 = self.basic.edu_1.apply(lambda x:str(x).split('/ '))
+        self.basic.edu_2 = self.basic.edu_2.apply(lambda x:str(x).split('/ '))
+
+
+    def build_experience(self, tags, max_list = 5):
+        '''
+            we'll use this section to build our experience section in the resume
+        '''
+        
+        # Choose all experience that contains at least 1 of the listed tags
+        job_lookup = self.jobs[self.jobs.tags.apply(lambda x: any(k in x for k in tags))]
+        company_list = job_lookup[job_lookup.type == 'J'].company.to_list()
+        experience = dict()
+        work = dict() #will get put in experience later        
+        count = 1
+        for company in company_list:
+            if count > max_list :
+                experience.update({'work': work})
+                return experience
+            
+            count += 1
+            row = job_lookup[job_lookup.company == company]
+            start_date = month[pd.to_datetime(row.start).dt.month.astype(int).item()] + ' ' + pd.to_datetime(row.start).dt.year.astype(str).item()
+            end_date = month[pd.to_datetime(row.end).dt.month.astype(int).item()] + ' ' + pd.to_datetime(row.end).dt.year.astype(str).item()
+            
+            work[company] = {
+                'location' : row.location.item(), 
+                'title' : row.title.item(), 
+                'date' : start_date + ' - ' + end_date,
+                'detail' : row.detail.item()
+            }
+            
+        experience['work'] = work
+        projects = dict() #will get put in experience later
+        project_list = job_lookup[job_lookup.type == 'P'].title.to_list()
+
+        for project in project_list:
+            if count > max_list : 
+                 break
+            count +=1
+            row = job_lookup[job_lookup.title == project]
+            start_date = month[pd.to_datetime(row.start).dt.month.astype(int).item()] + ' ' + pd.to_datetime(row.start).dt.year.astype(str).item()
+            try:
+                end_date = month[pd.to_datetime(row.end).dt.month.astype(int).item()] + ' ' + pd.to_datetime(row.end).dt.year.astype(str).item()
+            except: 
+                end_date = "Ongoing"
+            projects[project] = {
+                'date' : start_date + ' - ' + end_date,
+                'detail' : row.detail.item()
+            }
+        experience['projects'] = projects
+        return experience
+                
+    def build_skills(self, max_list = 5):
+        ## May implement some classification algorithm in the future
+        skills = dict()
+        count = 1
+        for col in self.skills.columns:
+            skill_list = self.skills[col].item().split(', ')
+            if len(skill_list) > max_list:
+                while(len(skill_list) > max_list):
+                    skill_list.pop(-1)
+            skill_list=', '.join(skill_list)
+            skname = col + ': '
+            skills[skname] = skill_list
+        return skills
+    
+    def build_resume(self, template, keys, output, max_experience = 7, max_skills = 4):
+        name = self.basic.name.item()
+        education = {
+            self.basic.edu_1.item()[0] : self.basic.edu_1.item()[1],
+            self.basic.edu_2.item()[0] : self.basic.edu_2.item()[1]
+        }
+        address = self.basic.address.item()
+        experience = self.build_experience(tags = keys, max_list = max_experience) 
+        skills = self.build_skills(max_list=max_skills)
+        template.fill_resume(name, address, skills, experience['work'], education, experience['projects'])
+        template.output(output)
+        print('resume made')
+
+if __name__ == "__main__":
+    job_file = 'experience.csv'
+    skill_file = 'skills.csv'
+    language_file = 'languages.csv'
+    basic_info = 'basic_info.csv'
+    template = template_1()
+    resume = resume_builder(job_file, skill_file, language_file, basic_info)
+    key = ['programming']
+    fname = 'resume.pdf'
+    resume.build_resume(template, key, fname, max_experience = 6)
---
+++ templates.py
@@ -0,0 +1,181 @@
+'''
+Next Priority : Let's making something that will make our PDF
+    We'll make it barebones to start with : 
+        Let's have at most 5 sections:
+            - Header, Skills, Work Experience, Relevant Projects, Education 
+'''
+
+from fpdf import FPDF
+
+class template_1(FPDF):
+    '''
+        Default Colors: 
+            Headers (Blue) : 66, 81, 245 
+            Texts (Grey) : 39, 39, 46
+        
+        Let's assume that all of our data is going to be thrown in as a dictionary --
+
+        input format: 
+            jobs = { 
+                company : {
+                    'location' : string,
+                    'title' : string,
+                    'date' : string (month year),
+                    'detail' : list },
+            }
+
+            projects ={
+                project name : {
+                    'date' : string (month year), 
+                    'detail' : list,
+                } 
+            }
+
+            skills = {
+                skill_1 : string,
+                skill_2 : string,
+                skill_3 : string,..., 
+                skill_n : string
+            }
+    '''
+    
+    def head(self, name, address, font = 'Helvetica'):
+        self.font = font
+        self.set_font(self.font, 'B', 20)
+            
+        # Calculate width of title and position
+        w = self.get_string_width(name) + 6
+        self.set_x((210 - w) / 2)
+        
+        # Title
+        self.set_text_color(66, 81, 245)
+        self.cell(w, 7, txt = name, align = 'C', new_y = 'NEXT')
+        self.set_font(self.font, 'I', 11)
+        self.ln(0.5)
+        w = self.get_string_width(address) + 6
+        self.set_x((210 - w) / 2)
+        
+        ## Subsection Font
+        self.set_text_color(39, 39, 46)
+        self.cell(w, 5, address, align = 'C', new_y = "NEXT", new_x = "LMARGIN")
+        
+        ## Divider
+        x = self.get_x()
+        y = self.get_y()
+        self.line(x, y+2, 195, y+2)
+        self.ln(6)
+        
+    def section_header(self, header):
+        self.set_font(self.font, 'B', 16)
+        # Background color
+        self.set_fill_color(255, 255, 255)
+        self.set_text_color(66, 81, 245)
+        
+        # Title
+        self.cell(0, 6, header, align = 'L', new_x = "LMARGIN", new_y = "NEXT")
+        x = self.get_x()
+        y = self.get_y()
+        self.line(x, y, 200, y)
+        self.ln(1.5)
+        self.set_text_color(39, 39, 46)
+
+    def skills(self, skills): 
+        header = 'Skills'
+        self.section_header(header)
+        for skill in skills:
+            self.set_font(self.font, 'B', 12)
+            #self.set_x(15)
+            
+            self.cell(0, 6, skill, align = 'L')
+            self.set_font(self.font, 'I', 12)
+            w = self.get_string_width(skill)
+            self.set_x(w + 15)
+            self.multi_cell(0, 6, skills.get(skill), new_x = "LMARGIN", new_y = "NEXT", align = 'L')
+        self.ln(2)
+        
+    def work_exp(self, w_exp):
+        header = 'Professional Experience'
+        self.section_header(header)
+        
+        for company in w_exp:
+            info = w_exp.get(company)
+            date_location = info.get('date') + ', ' + info.get('location')
+            title = info.get('title')
+            self.set_font(self.font, 'B', 12)
+            
+            self.cell(0, 6, title, align = 'L', new_y = "NEXT", new_x="LMARGIN")
+            self.cell(0, 6, company, align = 'L')
+            self.set_font(self.font, 'I', 12)
+            self.cell(0, 6, txt = date_location, align = 'R', new_x = "LMARGIN", new_y = "NEXT")
+            self.set_font(self.font, '', 12)
+            for detail in info.get('detail'):
+                self.set_x(self.get_x() + 5)
+                self.multi_cell(0, 6, txt = ('\u00b7 '+detail), align = 'L', new_y = "NEXT", new_x = "LMARGIN")
+            
+        self.ln(2)
+
+    def proj_exp(self, p_exp):
+        header = 'Relevant Projects'
+        self.section_header(header)
+        for project in p_exp:
+            info = p_exp.get(project)
+            date = info.get('date')
+            self.set_font(self.font, 'B', 12)
+            
+            self.cell(0, 6, project, align = 'L')
+            self.set_font(self.font, 'I', 12)
+            self.cell(0, 6, txt = date, align = 'R', new_x = "LMARGIN", new_y = "NEXT")
+            self.set_font(self.font, '', 12)
+            for detail in info.get('detail'):
+                self.set_x(self.get_x() + 5)
+                self.multi_cell(0, 6, txt = ('\u00b7 '+detail), align = 'L', new_y = "NEXT", new_x = "LMARGIN")
+        self.ln(2)
+
+    def education(self, education):
+        header = 'Education'
+        self.section_header(header)
+        
+        for degree in education:
+            self.set_font(self.font, 'B', 12)
+            #self.set_x(15)
+            self.cell(0, 6, degree, new_x = "LMARGIN", new_y = "NEXT", align = 'L')
+            self.set_font(self.font, '', 12)
+            #self.set_x(15)
+            self.cell(0, 6, education.get(degree), new_x = "LMARGIN", new_y = "NEXT", align = 'L')
+            self.ln(1.5)
+
+        self.ln(2)
+    
+    def fill_resume(self, name, address, skills, w_exp, edu, p_exp = None):
+        self.add_page()
+        self.head(name, address)
+        if p_exp: 
+            self.proj_exp(p_exp)
+        self.work_exp(w_exp)
+        self.skills(skills)
+        self.education(edu)
+
+
+if __name__=="__main__":
+    name = 'Raymond Yung'
+    address = 'Philadelphia, PA, 19121 | 516.469.0726 | Raymond.Yung@drexel.edu | https://rayyungdev.github.io'
+    skills = {'Programming Languages:': 'Python, Javascript, Matlab, Powershell, C++', 'Software & Tools:' : 'Git, Powershell, Pytorch'}
+
+    ## Let's assume that work experience & projects are list of lists
+    exp = {
+        'Health Partner Plans' : {'location' : 'Philadelphia, PA', 'title' : 'Privacy and Security Intern', 'date' : 'April 2019 - December 2019', 'detail' : ['Worked with company governance software to manage data access', 'Automated daily reports with Powershell to save manpower', 'Cooperated with IT Staff to promote cybersecurity and role governance within company']}
+    }
+
+    edu = {
+        'Masters of Science in Electrical Engineering' : 'Drexel University, Philadelphia PA, Completed June 2022, GPA: 3.68',
+        'Bachelors of Science in Electrical Engineering' : 'Drexel University, Philadelphia PA, Completed June 2020, GPA: 3.00'
+    }
+
+    p_exp = {
+        'Resume Builder' : {'date' : 'July 2022 - August 2022', 'detail' : ['Purpose is to create my own resumes based off job intention', 'created a database that is updated with my current history']},
+        'Interview Bot' : {'date' : 'July 2022 - Ongoing', 'detail' : ['Purpose is for potential recruiters to interview me', 'Created an interview database that consists of over 100 questions', 'Uses a model for Intent Classification to understand user intention']}
+    }
+    resume = template_1()
+    resume.fill_resume(name, address, skills, exp, edu, p_exp)
+
+    resume.output('template_1.pdf')
