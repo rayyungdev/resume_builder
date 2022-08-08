e7de12ced1c4e421ff19f473cdf5de64d8174d8a
Raymond <thec0okierebel12@gmail.com>
2022-08-08 17:56:15 -0400

    fixed main.py to accept command line arguments

--- builder.py
+++ builder.py
@@ -6,33 +6,19 @@ month = {
     1 : 'January', 2 : 'February', 3 : 'March', 4 : 'April', 5 : 'May', 6 : 'June', 7 : 'July', 8 : 'August', 9 : 'September', 10 : 'October', 11 : 'November',12 : 'December'    
 }
 
-class resume_builder:
-    def __init__(self, fjobs, fskills, basic_info):
-        self.jobs = pd.read_csv(fjobs)
-        self.jobs['start'] = pd.to_datetime(self.jobs['start'])
-        self.jobs['end'] = pd.to_datetime(self.jobs['end'])
-        self.jobs = self.jobs.sort_values('start', ascending = False)
-        self.jobs.tags = self.jobs.tags.apply(lambda x: x.split(', '))
-        self.jobs.detail = self.jobs.detail.apply(lambda x: x.split(', '))
-        
-        self.skills = pd.read_csv(fskills)
-        # for col in self.skills.columns:
-        #     self.skills[col] = self.skills[col].apply(lambda x: str(x).split(', '))
-
-        # self.language = pd.read_csv(flanguage)
-        # for col in self.language.columns:
-        #     self.language[col] = self.language[col].apply(lambda x: str(x).split(', '))
-
-        self.basic = pd.read_csv(basic_info)
-        self.basic.edu_1 = self.basic.edu_1.apply(lambda x:str(x).split('/ '))
-        self.basic.edu_2 = self.basic.edu_2.apply(lambda x:str(x).split('/ '))
+class builder: 
+    '''
+        my plan is to make this class inheritable for difference types of inputs. 
+    '''
     def build_experience(self, tags, max_list = 5, display_project_skills = False):
-        '''
-            we'll use this section to build our experience section in the resume
-        '''
-        
-        # Choose all experience that contains at least 1 of the listed tags
+        if type(self.jobs) is dict:
+            self.jobs = pd.DataFrame.from_dict(self.jobs, orient='index' )
+        self.jobs['start'] = pd.to_datetime(self.jobs ['start'])
+        self.jobs ['end'] = pd.to_datetime(self.jobs['end'])
+        self.jobs  = self.jobs.sort_values('start', ascending = False)
+        self.jobs['type'] = self.jobs['type'].apply(lambda x: x.upper())
         job_lookup = self.jobs[self.jobs.tags.apply(lambda x: any(k in x for k in tags))]
+
         company_list = job_lookup[job_lookup.type == 'J'].company.to_list()
         experience = dict()
         work = dict() #will get put in experience later        
@@ -81,123 +67,75 @@ class resume_builder:
                 }
         experience['projects'] = projects
         return experience
-                
-    def build_skills(self, max_list = 5):
-        ## May implement some classification algorithm in the future
-        skills = dict()
-        count = 1
-        for col in self.skills.columns:
-            skill_list = self.skills[col].item().split(', ')
-            if len(skill_list) > max_list:
-                while(len(skill_list) > max_list):
-                    skill_list.pop(-1)
-            skills[col] = skill_list
-        return skills
-    
-    def build_resume(self, template, keys, output, max_experience = 7, max_skills = 4, display_project_skills = False):
-        name = self.basic.name.item()
-        education = {
-            self.basic.edu_1.item()[0] : 
-            {
-                'address':self.basic.edu_1.item()[1], 
-                'completed': self.basic.edu_1.item()[2],
-                'GPA': self.basic.edu_1.item()[3]
+        
 
-            },
-            self.basic.edu_2.item()[0] : 
-            {
-                'address': self.basic.edu_2.item()[1],
-                'completed': self.basic.edu_2.item()[2],
-                'GPA': self.basic.edu_2.item()[3]
-            }
-        }
-        address = self.basic.address.item()
+    def build_skills(self, max_list = 5):
+        for skill in self.skills:
+            skill_set = self.skills[skill]
+            if len(skill_set) < max_list:
+                continue
+            else: 
+                self.skills[skill] = skill_set[0:max_list]
+        return self.skills
+    
+    def build_resume(self, template, keys, output, max_experience = 7, max_skills = 7, display_project_skills = False):
+        name = self.name
+        address = self.address
+        education = self.education
         experience = self.build_experience(tags = keys, max_list = max_experience, display_project_skills= display_project_skills) 
         skills = self.build_skills(max_list=max_skills)
         template.fill_resume(name, address, skills, experience['work'], education, experience['projects'])
         template.output(output)
         print('resume made')
 
+
+class csv_builder(builder):
+    def __init__(self, fjobs, fskills, basic_info):
+        self.jobs = pd.read_csv(fjobs)
+        self.jobs.tags = self.jobs.tags.apply(lambda x: x.split(', '))
+        self.jobs.detail = self.jobs.detail.apply(lambda x: x.split(', '))
+
+        basic = pd.read_csv(basic_info)
+        basic.edu_1 = basic.edu_1.apply(lambda x:str(x).split('/ '))
+        basic.edu_2 = basic.edu_2.apply(lambda x:str(x).split('/ '))
+        self.education = {
+            basic.edu_1.item()[0] : 
+            {
+                'address':basic.edu_1.item()[1], 
+                'completed': basic.edu_1.item()[2],
+                'GPA': basic.edu_1.item()[3]
+
+            },
+            basic.edu_2.item()[0] : 
+            {
+                'address': basic.edu_2.item()[1],
+                'completed': basic.edu_2.item()[2],
+                'GPA': basic.edu_2.item()[3]
+            }
+        }
+        self.address = basic.address.item()
+
+        self.skills = pd.read_csv(fskills)
+        for col in self.skills:
+            self.skills[col] = self.skills[col].apply(lambda x: x.split(', '))
+        self.skills = self.skills.to_dict()
+        for i in self.skills:
+            self.skills[i] = self.skills[i][0]
+        self.name = basic.name.item()
+
+
 import yaml
 
-class yaml_builder: 
+class yaml_builder(builder): 
     def __init__(self, fname):
         with open (fname, 'r') as f:
             self.data = yaml.safe_load(f)
-    
-    def build_skills(self, key_search = None, max_list = 5):
-        # for skill in self.data['skills']:
-        #     print(self.data['skills'].get(skill))
-        return self.data['skills']
-    
-    def build_experiences(self, tags, max_list = 7, display_project_skills = False):
-        experience_df = pd.DataFrame.from_dict(self.data['experience'], orient='index' )
-        experience_df ['start'] = pd.to_datetime(experience_df ['start'])
-        experience_df ['end'] = pd.to_datetime(experience_df['end'])
-        experience_df  = experience_df.sort_values('start', ascending = False)
+        self.jobs = self.data['experience']
+        self.skills = self.data['skills']
+        self.name = self.data['name']
+        self.address = self.data['subheader_info']
+        self.education = self.data['education']
 
-        job_lookup = experience_df[experience_df.tags.apply(lambda x: any(k in x for k in tags))]
-
-        company_list = job_lookup[job_lookup.type == 'J'].company.to_list()
-        experience = dict()
-        work = dict() #will get put in experience later        
-        count = 1
-        for company in company_list:
-            if count > max_list :
-                experience.update({'work': work})
-                return experience
-            
-            count += 1
-            row = job_lookup[job_lookup.company == company]
-            start_date = month[pd.to_datetime(row.start).dt.month.astype(int).item()] + ' ' + pd.to_datetime(row.start).dt.year.astype(str).item()
-            end_date = month[pd.to_datetime(row.end).dt.month.astype(int).item()] + ' ' + pd.to_datetime(row.end).dt.year.astype(str).item()
-            
-            work[company] = {
-                'location' : row.location.item(), 
-                'title' : row.title.item(), 
-                'date' : start_date + ' - ' + end_date,
-                'detail' : row.detail.item()
-            }
-            
-        experience['work'] = work
-        projects = dict() #will get put in experience later
-        project_list = job_lookup[job_lookup.type == 'P'].title.to_list()
-
-        for project in project_list:
-            if count > max_list : 
-                 break
-            count +=1
-            row = job_lookup[job_lookup.title == project]
-            start_date = month[pd.to_datetime(row.start).dt.month.astype(int).item()] + ' ' + pd.to_datetime(row.start).dt.year.astype(str).item()
-            try:
-                end_date = month[pd.to_datetime(row.end).dt.month.astype(int).item()] + ' ' + pd.to_datetime(row.end).dt.year.astype(str).item()
-            except: 
-                end_date = "present"
-            if not display_project_skills:
-                projects[project] = {
-                    'date' : start_date + ' - ' + end_date,
-                    'detail' : row.detail.item()
-                }
-            else: 
-                projects[project] = {
-                    'date' : start_date + ' - ' + end_date,
-                    'detail' : row.detail.item(),
-                    'skills': row.skills.item()
-                }
-        experience['projects'] = projects
-        return experience
-
-    def build_resume(self, template, keys, output, max_experience = 7, max_skills = 4, display_project_skills = False):
-        name = self.data['name']
-        address = self.data['subheader_info']
-        education = self.data['education']
-
-        skills = self.build_skills()
-        experience = self.build_experiences(tags = keys, display_project_skills=display_project_skills)
-        skills = self.build_skills(max_list=max_skills)
-        template.fill_resume(name, address, skills, experience['work'], education, experience['projects'])
-        template.output(output)
-        print('resume made')
 if __name__ == "__main__":
     fname = './data/data.yaml'
     test = yaml_builder(fname)
--- main.py
+++ main.py
@@ -1,22 +1,91 @@
+from xmlrpc.client import Boolean
 from builder import *
 from templates import *
-import sys, getopt
+import argparse
+
 '''
     Eventual Goal for this file is to run everything to the command line    
 '''
 
-job_file = './data/experience.csv'
-skill_file = './data/skills.csv'
-basic_info = './data/basic_info.csv'
+data_yaml = './data/data.yaml'
 
-#### Template_basic no skills
-template = template_basic()
-resume = resume_builder(job_file, skill_file, basic_info)
-key = ['programming']
-fname = 'template_basic_1.pdf'
-resume.build_resume(template, key, fname, max_experience = 7, display_project_skills = False) 
+# template = template_basic()
+# resume = csv_builder(job_csv, skill_csv, basic_csv)
+# key = ['programming']
+# fname = 'template_basic_1.pdf'
+# resume.build_resume(template, key, fname, max_experience = 7, display_project_skills = False) 
 
-template = template_basic()
-resume_2 = resume_builder(job_file, skill_file, basic_info)
-fname = 'template_basic_2.pdf'
-resume_2.build_resume(template, key, fname, max_experience = 7, display_project_skills=True)
+# template = template_basic()
+# resume = csv_builder(job_csv, skill_csv, basic_csv)
+# fname = 'template_basic_2.pdf'
+# resume_2.build_resume(template, key, fname, max_experience = 7, display_project_skills=True)
+
+'''
+   Arguments:
+      - data 
+      - template
+      - max_experience
+      - max_skills
+      - display_project_skills 
+      - file name
+      - keys
+'''
+my_parser = argparse.ArgumentParser(description='Resume Generator based off Key')
+my_parser.add_argument('--input','-i', nargs = '*', action = 'store', type = str, required = False, help = 'Input Data')
+my_parser.add_argument('--fname','-f', action = 'store', type = str, required = True, help = 'file name')
+my_parser.add_argument('--key','-k', action = 'store', nargs = '+', type = str, required = True, help = 'keys')
+my_parser.add_argument('--template','-t',action = 'store', type = str, required = False, help ='template type (only basic available)')
+my_parser.add_argument('--max_experience', '-me', type= int, required = False, help = 'maximum experience, default = 7')
+my_parser.add_argument('--max_skills', '-ms', type =int, required=False, help = 'maximum skills shown in tab, default = 7')
+my_parser.add_argument('--display_project_skills', '-dps', type = Boolean, required=False, help = 'show skills in job section, default = False')
+
+args = vars(my_parser.parse_args())
+input = args['input']
+template = args['template']
+me = args['max_experience']
+ms = args['max_skills']
+dps = args['display_project_skills']
+fname = args['fname']
+key = args['key']
+
+if not fname.endswith('.pdf'):
+   fname = fname + '.pdf'
+if input is None:
+   input = [data_yaml]
+
+if template is None: 
+   template = template_basic()
+
+if me is None:
+   me = 7
+
+if ms is None:
+   ms = 7
+
+resume = False
+
+if len(input) == 1:
+   input = input[0]
+   if input.endswith('.yaml'):
+      resume = yaml_builder(input)
+   else: 
+      print('invalid argument' + input)
+else:
+   for file in input:
+      check = 0
+      if 'basic_info.csv' in file:
+         basic_csv = file
+      elif 'experience.csv' in file:
+         job_csv = file
+      elif 'skills.csv' in file:
+         skills_csv = file 
+      else:
+         print('Invalid Argument : ', file)
+         check +=1
+   if check == 0:
+      resume = csv_builder(job_csv, skills_csv, basic_csv)
+if dps is None:
+   dps = False
+
+if resume:
+   resume.build_resume(template, key, fname, max_experience = me, max_skills = ms, display_project_skills=dps)
